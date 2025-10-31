00f6d2da56dfc937ce5d34d03b5c3902f2a96d71
Broker0 <brok79@internet.ru>
2025-11-01 00:30:22 +0500

    example for events

---
+++ examples/events.py
@@ -0,0 +1,60 @@
+import asyncio
+import random
+from datetime import datetime
+
+from py_astealth.api_client import AsyncStealthApiClient
+from py_astealth.stealth_client import EventType
+from py_astealth.examples.example_lib import graceful_shutdown
+from py_stealth.protocol import get_port
+
+
+@graceful_shutdown
+async def event_handler(client: AsyncStealthApiClient):
+    await client.AddToSystemJournal("event handler started")
+    while True:
+        event = await client.events.get()
+        print(event)
+
+
+@graceful_shutdown
+async def clicker(client: AsyncStealthApiClient):
+    await client.AddToSystemJournal("clicker handler started")
+    while True:
+        # click every 2-5 seconds
+        await asyncio.sleep(random.random()*3.0+2.0)
+        # await client.FindTypeEx(0xFFFF, 0xFFFF, 0, True)
+        # item = random.choice(await client.GetFindedList())
+        item = await client.GetSelfID()
+        await client.ClickOnObject(item)
+
+
+async def main():
+    client = AsyncStealthApiClient("127.0.0.1", get_port())
+    try:
+        await client.connect()
+        await client.SetEventCallback(EventType.EvSpeech)
+
+        _tasks = [
+            asyncio.create_task(clicker(client)),
+            asyncio.create_task(event_handler(client)),
+        ]
+
+        done, pending = await asyncio.wait(_tasks, return_when=asyncio.FIRST_COMPLETED)
+
+        print(f"{len(done)} task(s) completed. Initiating shutdown...")
+
+    except ConnectionRefusedError:
+        print("Connection refused. Make sure Stealth client is running.")
+    finally:
+        if '_tasks' in locals():
+            for task in _tasks:
+                if not task.done():
+                    task.cancel()
+
+            # waiting complete task
+            await asyncio.gather(*_tasks, return_exceptions=True)
+
+        client.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ examples/example_lib.py
@@ -0,0 +1,21 @@
+import asyncio
+import functools
+from py_astealth.api_client import AsyncStealthApiClient
+
+
+def graceful_shutdown(coro):
+    @functools.wraps(coro)
+    async def wrapper(client: AsyncStealthApiClient, *args, **kwargs):
+        task_name = coro.__name__
+        await client.AddToSystemJournal(f"{task_name.capitalize()} started")
+        try:
+            return await coro(client, *args, **kwargs)
+        except asyncio.CancelledError:
+            await client.AddToSystemJournal(f"{task_name.capitalize()} cancelled")
+            raise
+        except Exception as e:
+            print(f"[!] Exception in {task_name.capitalize()}: {e}")
+        finally:
+            await client.AddToSystemJournal(f"{task_name.capitalize()} completed")
+
+    return wrapper
