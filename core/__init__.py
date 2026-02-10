21227e87b9dba907a152b478eb314fa33fd9bf57
Broker0 <brok79@internet.ru>
2026-02-10 15:42:19 +0500

    init and all for core module

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+from py_astealth.core.base_types import MethodSpec, ParameterSpec, StructType, PrimitiveType
+from py_astealth.core.api_specification import ApiSpecification
+from py_astealth.core.rpc_client import AsyncRPCClient
+
+
+__all__ = [
+    'MethodSpec', 'ParameterSpec',
+    'StructType', 'PrimitiveType',
+    'ApiSpecification',
+    'AsyncRPCClient',
+]
