9b3bc53a9846066b35d2539585b0d1fbd11db563
Broker0 <brok79@internet.ru>
2026-03-04 23:06:12 +0500

    optimize imports

--- stealth/api.py
+++ stealth/api.py
@@ -10,11 +10,12 @@ from py_astealth.stealth_api import StealthApi
 from ._internals import _create_global_proxy
 
 from py_astealth.generated.sync_module import *
+from py_astealth.generated import sync_module as _sync_module
 
 # Auto-generate synchronous wrappers for all API methods
-__all__ = []
+globals().update({
+    spec.name: _create_global_proxy(spec) for spec in StealthApi.get_methods()
+})
 
-for spec in StealthApi.get_methods():
-    # Create a global wrapper function for each API method
-    globals()[spec.name] = _create_global_proxy(spec)
-    __all__.append(spec.name)
+
+__all__ = sorted(set(_sync_module.__all__))
