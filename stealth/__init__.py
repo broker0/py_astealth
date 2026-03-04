cddc51e1fceb64ded45f14099a8edb7d7e68f105
Broker0 <brok79@internet.ru>
2026-03-04 21:27:28 +0500

    optimize import

--- stealth/__init__.py
+++ stealth/__init__.py
@@ -4,24 +4,17 @@ from py_astealth.generated.sync_module import *
 from py_astealth.stealth._internals import _create_global_proxy
 from py_astealth.stealth_api import StealthApi
 
-from py_astealth.stealth import methods
-from py_astealth.generated import sync_module
-
-def _populate_module():
-    """
-    Dynamically creates and adds all API functions to this module.
-    """
-    current_module = globals()
-    specs = StealthApi.get_methods()
-
-    for spec in specs:
-        proxy_function = _create_global_proxy(spec)
-        current_module[spec.name] = proxy_function
+from py_astealth.stealth import methods as _methods
+from py_astealth.generated import sync_module as _sync_module
 
 
-_populate_module()
+# binding real proxy functions to stubs
+globals().update({
+    spec.name: _create_global_proxy(spec) for spec in StealthApi.get_methods()
+})
 
 # Import helpers, overriding any API methods if names collide (e.g. AddToSystemJournal)
 from py_astealth.stealth.methods import *
 
-__all__ = sorted(list(set(sync_module.__all__ + methods.__all__)))
+
+__all__ = sorted(set(_sync_module.__all__) | set(_methods.__all__))
