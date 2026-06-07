a388f732bf1c188b6d5be23363b00e7675d3acb6
Lorenzo P. <43588107+ilpersi@users.noreply.github.com>
2026-06-07 16:52:53 +0200

    Fast loop - initial commit.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 __author__ = 'Broker'
 
+from py_astealth.utilites.fast_loop import install as _install_fast_loop
+_install_fast_loop()
+
 from py_astealth.async_client import AsyncStealthApiClient
 from py_astealth.stealth_session import StealthSession
 from py_astealth.async_pool import AsyncClientPool
